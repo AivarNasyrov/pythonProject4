d8c619390845cd16f2bd93b3f94ece055e50e64c
AivarNasyrov ON GITHUB <dom.nasyrovyh1@gmail.com ON GITHUB>
2023-03-02 18:57:51 +0600

    hw 6 is done

---
+++ hw_6.py
@@ -0,0 +1,40 @@
+# #[a-f0-9]+ colors
+# ^(?:[A-Za-z]+[-\']?[A-Za-z]?[A-za-z]+\s)+ names and surnames
+# \s[A-Za-z]+\.[a-z0-9]+ file names
+# \s[A-Za-z0-9]+\@[a-z0-9]+[\.{2}a-z-]+ emails
+
+import re
+
+while True:
+    with open('MOCK_DATA.txt', 'r') as file:
+        content = file.read()
+    search_input = int(input('Names - 1, Colors - 2, files - 3, emails - 4, exit - 5'))
+    if search_input == 1:
+
+        names_list = re.findall(r'^(?:[A-Za-z]+[-\']?[A-Za-z]?[A-za-z]+\s)+', content)
+        print(names_list)
+        with open('names.txt', 'w') as file_1:
+            for name in names_list:
+                file_1.write(f'{name}\n')
+    elif search_input == 2:
+        colors_list = re.findall(r'#[a-f0-9]+ colors', content)
+        with open('colors.txt', 'w') as file_2:
+            for color in colors_list:
+                file_1.write(f'{color}\n')
+
+    elif search_input == 3:
+        files_list = re.findall(r'(?:\s[?:A-Za-z]+\.[a-z0-9]+])+', content)
+        with open('files.txt', 'w') as file_3:
+            for file in files_list:
+                file_1.write(f'{file}\n')
+
+    elif search_input == 4:
+        emails_list = re.findall(r'\s[A-Za-z0-9]+\@[a-z0-9]+[\.{2}a-z-]+', content)
+        with open('files.txt', 'w') as file_3:
+            for email in emails_list:
+                file_1.write(f'{email}\n')
+    elif search_input == 5:
+        break
+
+    else:
+        print("don't print other numbers except 1, 2, 3, 4, 5")
--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+text = 'AV Analytics Vidhya AV'
 
+result = re.match(r'AV', text)
+print(result)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+result = re.match(r'.V', text)
+print(result)
 
+result = re.match(r'Analytics', text)
+print(result)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+result = re.search(r'Analytics', text)
+print(result)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+result = re.search(r'AV', text)
+print(result)
+
+result = re.findall(r'AV', text)
+print(result)
+
+result = re.findall(r'[Aa].', text)
+print(result)
+
+result = re.split(r' ', text)
+print(result)
+
+result = re.split(r'[AVav]', text)
+print(result)
+
+result = re.sub(r'[AOIUEYaoiuye]', r'#', text)
+print(result)
+
+with open('test_regs.txt', 'r', encoding='utf-8') as file:
+    content = file.read()
+
+    beeline_list = re.findall(r'\+996 (?:77\d|22[0-57])[0-9 ]{9}', content)
+    print(f'{len(beeline_list)}: {beeline_list}')
+
+    mega_list = re.findall(r'\+996 (?:55\d|755|99[7-905])[0-9 ]{9}', content)
+    print(f'{len(mega_list)}: {mega_list}')
+
+    o_list = re.findall(r'\+996 (?:70\d|50[0-27-945])[0-9 ]{9}', content)
+    print(f'{len(o_list)}: {o_list}')
